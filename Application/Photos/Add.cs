d3d2b790dd6367645309bf25dbfb846fb446fb69
Pearel Nazareth <pearel@noesyssoftware.com>
2020-03-24 17:06:33 +0530

    here is chandler

--- Application/Photos/Add.cs
+++ Application/Photos/Add.cs
@@ -2,6 +2,28 @@ namespace Application.Photos
 {
     public class Add
     {
-    
+        public class Command : IRequest
+                {
+                    
+                }
+        
+                public class Handler : IRequestHandler<Command>
+                {
+                    private readonly DataContext _context;
+                    public Handler(DataContext context)
+                    {
+                        _context = context;
+                    }
+        
+                    public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+                    {
+                        //handler logic goes here
+                        var success = await _context.SaveChangesAsync() > 0;
+        
+                        if(success) return Unit.Value;
+        
+                        throw new Exception("Problem saving changes");
+                    }
+                }
     }
 }
